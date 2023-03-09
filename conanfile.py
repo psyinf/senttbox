4da743dbf2a7414622c18cfb7084777164045d26
psy_inf <sebastian@smesserschmidt.de>
2023-03-10 00:40:35 +0100

    imgui example setup

--- conanfile.py
+++ conanfile.py
@@ -15,11 +15,11 @@ class MSGE(ConanFile):
                 ,("modern-cpp-kafka/2022.12.07")
                 ,("entt/3.11.1")
                 ,("cereal/1.3.2")
-                #,("imgui/cci.20230105+1.89.2.docking")
-                #,("vulkan-headers/1.3.239.0")
-                #,("vulkan-loader/1.3.239.0")
-                #,("glfw/3.3.2")
-               # ,("glew/2.1.0")
+                ,("imgui/cci.20230105+1.89.2.docking")
+                ,("vulkan-headers/1.3.239.0")
+                ,("vulkan-loader/1.3.239.0")
+                ,("glfw/3.3.2")
+                ,("glew/2.1.0")
                 ]
     generators = "cmake_find_package_multi"
     
